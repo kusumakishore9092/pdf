bb57487baf8961e618602d24f893f3a642f40c19
kusumanjalikoneru <>
2018-03-11 22:09:07 -0500

    add image to pdf

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const Handlebars = require('handlebars');
 
 const source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
              "{{kids.length}} kids:</p>" +
-             "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
-const template = Handlebars.compile(source);
+             "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>" +
+             "<img src='http://localhost:3000/img.jpg' alt='Smiley face' height='42' width='42'>";
 
+const template = Handlebars.compile(source);
 const data = { "name": "Alan", "hometown": "Somewhere, TX",
              "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
 const result = template(data);
@@ -21,8 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/download', (req,res) => {
   pdf.create(result, options).toStream(function(err, stream) {
     if (err) return console.log(err);
-      res.setHeader('Content-type', 'application/pdf')
-        stream.pipe(res)
+      res.setHeader('Content-type', 'application/pdf');
+      stream.pipe(res);
   });
 });
 
